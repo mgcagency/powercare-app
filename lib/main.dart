3dffe12b6bfdce417f2c1c3f4ea93379a3742d9e
Komal B <komal@mgcagency.co.uk>
2026-06-29 17:00:40 +0530

    only mobile compatible

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'app/theme/colors.dart';
 import 'app/theme/text_styles.dart';
+import 'app_frame.dart';
 import 'core/navigation/app_navigator.dart';
 import 'features/presentation/landing/splash_screen.dart';
 import 'flavor_config.dart';
@@ -36,6 +37,11 @@ class PowerCareApp extends StatelessWidget {
       'Running Flavor: ${FlavorConfig.instance.name}',
     );
     return MaterialApp(
+      builder: (context, child) {
+        return AppFrame(
+          child: child!,
+        );
+      },
       title: FlavorConfig.instance.name,
       debugShowCheckedModeBanner: false,
       theme: appTheme,
